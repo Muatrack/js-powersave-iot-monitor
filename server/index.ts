64735ba2e97d361af01f795d12705f138d21b945
sernola <fjfhccfkuk@163.com>
2025-05-21 11:29:30 +0800

    update

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,25 @@
 export default {
-	fetch(request) {
-		const url = new URL(request.url);
+	async fetch(request, env) {
+    const { pathname } = new URL(request.url);
 
-		if (url.pathname.startsWith("/api/")) {
-			return Response.json({
-				name: "Cloudflare",
-			});
-		}
-		return new Response(null, { status: 404 });
-	},
+    if (pathname === "/api/db") {
+      // If you did not use `DB` as your binding name, change it here
+      const { results } = await env.DB.prepare(
+        "SELECT * FROM user_list"
+      )
+    	.bind("*")
+        .all();
+      return new Response(JSON.stringify(results), {
+        headers: { 'Content-Type': 'application/json' }
+      });
+    }
+
+	const url = new URL(request.url);
+	if (url.pathname.startsWith("/api/name")) {
+		return Response.json({
+			name: "Cloudflare",
+		});
+	}
+	return new Response(null, { status: 404 });
+  }
 } satisfies ExportedHandler<Env>;
